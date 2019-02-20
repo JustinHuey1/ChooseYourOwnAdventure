f7be951917b87094bb29e3a71ab30848a92762fc
Justin Huey <juhuey1@s.sfusd.edu>
2019-02-20 02:11:35 +0000

    Storyboard

--- js/game.js
+++ js/game.js
@@ -6,41 +6,104 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You are going on an expedition into space.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Travel to Mars",
+                    nextLevel: "Mars",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Travel to the moon",
+                    nextLevel: "Moon",
+                },
+                
+                {
+                    text: "Travel to the sun",
+                    nextLevel: "Sun",
                 },
             ]
         },
 
-        cave: {
+        Mars: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You spot an alien.",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Go and greet it.",
+                    nextLevel: "Greet",
+                },
+                
+                {
+                    text: "Stay in the spaceship and take pictures of the alien.",
+                    nextLevel: "Capture",
+                }
+            ]
+        },
+        
+        Greet: {
+            background_image: "",
+            music: "",
+            message: "Surprisingly, the alien can speak your language and welcomes you with open arms",
+            choices: [
+                {
+                    text: "Follow the alien to its household.",
+                    nextLevel: "Household",
+                },
+                
+                {
+                    text: "You get spooked and decide to go back to your spaceship",
+                    nextLeveL: "Capture",
+                }
+            ]
+        },
+        
+        Capture: {
+            background_image: "",
+            music: "",
+            message: "",
+            choices: 
+                {
+                    text: "Another alien comes up behind you and captures you",
+                    nextLevel: "Game_Over",
+                }
+        },
+        
+        Moon: {
+            background_image: "",
+            music: "",
+            message: "",
+            choices: [
+                {
+                    text: "",
+                    nextLevel: "",
                 },
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        Sun: {
+            background_image: "",
+            music: "",
+            message: "What did you think was going to happen...",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "You burn into a flame of fire",
+                    nextLevel: "Game_Over",
                 },
             ]
         },
-
-    }
+        
+        Game_Over: {
+            background_image: "",
+            music: "",
+            message: "It is doomed",
+            choices:
+                {
+                    text: "Start Over",
+                    nextLeveL: "start",
+                },
+            }
+        }   
+    
 };
